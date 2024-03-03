70576e8b820eb36c65cfce139b96f7a2f946489e
IHCantabraba <inigocanta@gmail.com>
2024-03-03 16:21:03 +0100

    ejercicios JS

--- index_Ej15.js
+++ index_Ej15.js
@@ -1,7 +1,6 @@
 /*Crea una función llamada swap que reciba un array y dos parametros que sean 
 indices del array. La función deberá intercambiar la posición de los valores de 
 los indices que hayamos enviado como parametro. Retorna el array resultante.
-
 Sugerencia de array:*/
 
 const fantasticFour = [
